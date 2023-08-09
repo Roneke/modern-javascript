be045b8d7fef66b1aac2a1c8bc3cb0e9793195a1
RONEKE BROWNE <lilsaint900@hotmail.com>
2023-08-08 20:53:27 -0400

    webpack install

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+// console.log("hello");
+var moment = require("moment");
+var datetime = moment().startOf("day").fromNow();
+console.log(datetime);
+
+// var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+// console.log(currentTime);
